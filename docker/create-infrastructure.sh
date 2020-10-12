9e2d8411d68ecf3a6bcb180446faafc570fbeac7
Marc Morera <yuhu@mmoreram.com>
2020-10-12 17:37:05 +0200

    Preloaded bus services
    - Deleted entrypoints cache:clear

--- docker/create-infrastructure.sh
+++ docker/create-infrastructure.sh
@@ -3,7 +3,6 @@
 cd /var/www
 rm -Rf var/*
 
-php bin/console cache:warmup --env=prod --no-debug --no-interaction
 php bin/console event-bus:infra:create --exchange=events --env=prod --no-debug --no-interaction --force
 
 sh $1
--- docker/crontab.sh
+++ docker/crontab.sh
@@ -3,6 +3,5 @@
 cd /var/www
 rm -Rf var/*
 
-php bin/console cache:warmup --env=prod --no-debug --no-interaction
 php bin/console apisearch-server:generate-crontab --env=prod --no-debug --no-interaction
 cron -f
